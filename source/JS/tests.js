7ecbc0c9ba20937c308aca684f279a3db66b06eb
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-09 21:24:15 -0400

    Change to JS testing for map test, now including Jquery

--- source/JS/tests.js
+++ source/JS/tests.js
@@ -1,3 +1,11 @@
+//Add jquery
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+//Hello World test
 QUnit.test("hello test", function(assert) 
 {
     assert.ok(1 == "1", "One does in fact equal 1");
