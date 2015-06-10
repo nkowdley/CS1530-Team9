ea65bd37094f161363c051b0a2619665fcdb7bcc
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-09 21:24:15 -0400

    Still fiddling with the tests, cant seem to get google API access on test

--- source/JS/tests.js
+++ source/JS/tests.js
@@ -1,6 +1,6 @@
 //Add jquery
 var script = document.createElement('script');
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
