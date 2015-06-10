703291a89ec95a1bca3fd444221d3b29a5c84865
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-09 21:24:15 -0400

    Still trying to get test to run correctly

--- source/JS/maps.js
+++ source/JS/maps.js
@@ -1,3 +1,14 @@
+//Indcludes
+var script = document.createElement('script');
+script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+script = document.createElement('script');
+script.src = '//maps.googleapis.com/maps/api/js?key=AIzaSyCYRCZ4qnZv2QpyPCbSX7vP1ZiKLsuX9gQ"';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 //Function will cuase the display to go to "Map View", as of now just google map location
 function gotoMap() 
 {
