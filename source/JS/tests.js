3ca72757740e47024abeaf06ec7b67b706ed7b42
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-09 21:24:15 -0400

    Typo fix

--- source/JS/tests.js
+++ source/JS/tests.js
@@ -14,7 +14,7 @@ QUnit.test("hello test", function(assert)
 QUnit.test("maps test", function(assert) 
 {
     //Gets script to be tested
-    $.getScript("maps.js", function(){
+    $.getScript("JS/maps.js", function(){
         var result = gotoMap();
         assert.ok(result, "gotoMap() returned a map obj that evalutes to true");
     });
