22fb3cc5e501efa80d74d096c8bb4fee7c160b33
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-09 21:14:34 -0400

    One last fix and comment to php Hello World test

--- source/phpTestDemo.php
+++ source/phpTestDemo.php
@@ -1,6 +1,8 @@
 <?php
+//Only meant to run on the server itself, local host configuration would likely need to be identical
 class StackTest extends PHPUnit_Framework_TestCase
 {
+    //Simple Hello World test for the framework
     public function testPHPUnit()
     {
         $this->assertEquals(0, 0);
