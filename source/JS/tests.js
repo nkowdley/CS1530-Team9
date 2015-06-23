703464310dbb1f5f28ea6b42b22c6153ee2085ca
Andrew Harper <graphitezeppelin@gmail.com>
2015-06-23 14:56:47 -0400

    Added facebook login and updated CSS, may have introduced maps bug. Testing API's is quirky as Facebook won't let me call from local computer but will from local host (xampp) while google is the opposite

--- source/JS/tests.js
+++ source/JS/tests.js
@@ -1,10 +1,3 @@
-//Add jquery
-var script = document.createElement('script');
-script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
 //Hello World test
 QUnit.test("hello test", function(assert) 
 {
@@ -13,9 +6,6 @@ QUnit.test("hello test", function(assert)
 
 QUnit.test("maps test", function(assert) 
 {
-    //Gets script to be tested
-    $.getScript("JS/maps.js", function(){
         var result = gotoMap();
         assert.ok(result, "gotoMap() returned a map obj that evalutes to true");
-    });
 });
