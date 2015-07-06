573c0c7840a39d0fe9b92ce86b9eb8fd0e6342e2
Neel Kowdley <nkowdley@gmail.com>
2015-07-06 16:47:56 -0400

    Add new table for profiles

--- source/PHP/maketable.php
+++ source/PHP/maketable.php
@@ -12,11 +12,17 @@
     die ("Could not connect to db: " . $db->connect_error);
   endif;
 
+  //drop tables
   $result = $db->query("drop table Posts");
-  $result = $db->query("create table Posts (id int, user char(30) not null, type char(30) not null, content char(30), location char(30))") or die ("Invalid: " . $db->error);
+  $result = $db->query("drop table Users");
+  //make tables
+  $result = $db->query("create table Posts (id int, user char(30) not null, type char(30) not null, content char(30), location char(30))") or die ("Invalid: " . $db->error);//Posts table
+  $result = $db->query("create table Users (id int, user char(30) not null, interests char(30) not null, education char(30))") or die ("Invalid: " . $db->error);//Users Table
 
-  $db->query('INSERT INTO Posts (id, user, type, content, location)VALUES("1", "neel","text","hello world", "Pittsburgh, PA")') or die ("Invalid: " . $db->error);
-  
+  //dummy data for testing
+  $db->query('INSERT INTO Posts (id, user, type, content, location)VALUES("1", "neel","text","hello world", "Pittsburgh, PA")') or die ("Invalid: " . $db->error); //test post
+  $db->query('INSERT INTO Users Users (id,user,interests,education)VALUES("1", "neel","football|movies","University of Pittsburgh")') or die ("Invalid: " . $db->error); //test for user info
+
   echo "<h3>Here is your data:</h3>";
   $query = "select * from Posts;";
   $result = $db->query($query);
@@ -29,5 +35,5 @@
     echo "<br/>";
   endfor;
   ?>
-  </body>
-  </html>
+</body>
+</html>
