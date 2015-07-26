a9464c559731a003aff4e3e7df93b899b9ad4dfe
Andrew Harper <graphitezeppelin@gmail.com>
2015-07-26 17:08:48 -0400

    fixed a major bug I hadn't noticed before. Did a thing with the pic window but its pretty ugly atm, needs CSS

--- source/JS/pics.js
+++ source/JS/pics.js
@@ -1,3 +1,6 @@
+//Global tracker of info boxes
+var infos = [];
+
 //Listener for form submission
 $("form[name=upload-form]").submit(function(){
     uploadPic($("#myfile").val(), function(){});
@@ -24,7 +27,6 @@ function uploadPic(filename, callback) //callback for testing
             }
             
             //Prepare data and upload
-<<<<<<< HEAD
             //TODO: handle facebook error better. 
             else
             {
@@ -36,33 +38,7 @@ function uploadPic(filename, callback) //callback for testing
                     {
                         //Set user id as local var
                         console.log("UserId = " + userId);
-=======
-            //TODO: pack in user info and lat/lng of the pic
-            else
-            {
-                //Get and set user info as local vars
-                
-                
-                //Set lng/lat as local vars
-                var lat = coords.lat();
-                var lng = coords.lng();
-                                
-                //Get file from upload form data. This will include the name
-                var files = $('#myfile')[0];               
-                var data = new FormData();                
-                var url =  "PHP/uploadpic.php";
-                var xhr = new XMLHttpRequest();  
-
-                //Add files to dataform 
-                jQuery.each(files.files, function (i, file) {
-                    data.append("myfile", file)
-                });     
-                
-                //Pack lat/lng into dataform
-                data.append("lat", lat);
-                data.append("lng", lng);
->>>>>>> master
-                        
+     
                         //Set lng/lat as local vars
                         var lat = coords.lat();
                         var lng = coords.lng();
@@ -91,7 +67,8 @@ function uploadPic(filename, callback) //callback for testing
                         if( this.readyState === 4 ) 
                         {
                             var result = this.responseText;
-                            alert(result); //FOR DEBUG ONLY
+                            //alert(result); //FOR DEBUG ONLY
+                            alert("Uploaded"); 
                             $('#myfile').val(null);
                         }
                         });
@@ -122,11 +99,7 @@ function validateUploadForm(filename)
     }
   
     //If pic not selected, alert and exit.
-<<<<<<< HEAD
     if(filename.lastIndexOf("png") !== filename.length-3) //accepts only png
-=======
-    if(filename.lastIndexOf("png") !== filename.length-3) //Accepts only png
->>>>>>> master
     {
         alert("Must provide .png file to be uploaded");
         return "pic selection failed";
@@ -135,15 +108,9 @@ function validateUploadForm(filename)
     return "success";
 }
 
-<<<<<<< HEAD
 
 //Gets all pic entries from the DB by calling getPics.php
 function getAllPics(callback) //asynchronous
-=======
-//Unimplmented, and may not be used. Possibly just pack the lat/lng into db
-//Embeds the Lat/Lng position got from the google api's into a pics exif tags
-function embedCoords(file, coords)
->>>>>>> master
 {
     //Ajax call to getPics.php
     $.ajax({
@@ -163,6 +130,9 @@ function populateMap() //uses asynchronous calls, but its return shouldn't actua
 {
     //Get list of all pics
     getAllPics(function(pics) { //more asynch hell 
+        //Make array of unique marker names
+        var marker = [];
+        
         //Iterate over list of pics
         for(var i=0; i<pics.length; i++)
         {
@@ -171,31 +141,47 @@ function populateMap() //uses asynchronous calls, but its return shouldn't actua
             //Get attributes from pic
             //**Whats up with relative paths? this file is already in CStest so why need to add?
             var path = "/CStest" + pic.picPath; //THIS IS FOR MY PERSONAL LOCAL HOSTING, NEEDS CHANGED FOR SERVER
+            //var path = pic.picPath; 
             var uploader = pic.uploaderId;
             
             //Make LatLng obj
             var myLatLng = new google.maps.LatLng(pic.picLat, pic.picLng);
                         
             //Generate infoWindow for pic
-            var infoHtml = '<div style="width: 100%; height:100%;">' +
+            var content = '<div style="width: 100%; height:100%;">' +
+            '<h1>' + uploader + '\'s pic</h1>' + 
             '<img style="width: 400px;" src="' + path + '"width></div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: infoHtml
-            });
-            
+
+            var infowindow = new google.maps.InfoWindow()
+                        
             //Place marker for pic
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: 'Title'
+            var marker = new google.maps.Marker({  
+                map: map, title: "pic", position: myLatLng  
             });
             
             //Add listener to open window on click
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.open(map,marker);
-            });
-            
-            
+            google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+                return function() {
+                    //Close the previous info-window 
+                    closeInfos();
+                    
+                    infowindow.setContent(content);
+                    infowindow.open(map,marker);
+                    
+                    //Keep the name to close next
+                    infos[0] = infowindow;
+                };
+            })(marker,content,infowindow));  
         }
     });
+}
+
+//Close info boxes
+function closeInfos(){
+    if(infos.length > 0){
+        //Detach the info-window from the marker .
+        infos[0].set("marker", null);
+        infos[0].close();
+        infos.length = 0;
+    }
 }
--- source/PHP/maketable.php
+++ source/PHP/maketable.php
@@ -4,12 +4,9 @@
   <title>Generating a Simple Table</title>
 </head>
 <body>
-  <?php
-<<<<<<< HEAD
-  $db = new mysqli('localhost', 'user', 'password', 'db');
-=======
-  $db = new mysqli('localhost', 'root', '', 'CS1530');
->>>>>>> master
+<?php
+  $db = new mysqli('localhost', 'user', 'password', 'db'); //yes I changed it in master again. this is on the server
+
   if ($db->connect_error):
     die ("Could not connect to db: " . $db->connect_error);
   endif;
@@ -19,23 +16,16 @@
   $result = $db->query("drop table Users");
   
   //make tables
-<<<<<<< HEAD
   //Pics table scheme changed to have arbitrary (auto incremented) id as primary key, and hold geolocation, uploader name/id and relative path to file
   $result = $db->query("create table Pics (id int NOT NULL AUTO_INCREMENT, uploaderId varchar(64) not null, picLat varchar(64), picLng varchar(64), picPath varchar(64), PRIMARY KEY(id))") or die ("Invalid: " . $db->error);//Pics table
   $result = $db->query("create table Users (id int, user varchar(64) not null, interests varchar(64) not null, education varchar(64), PRIMARY KEY (user))") or die ("Invalid: " . $db->error);//Users Table
  
   //dummy data for testing
-  //$db->query('INSERT INTO Pics (uploaderName, uploaderId, picGeolocation, picPath)VALUES("neel", "1", "text", "hello world")') or die ("Invalid: " . $db->error); //test post
+  $db->query("INSERT INTO Pics (uploaderId, picLat, picLng, picPath) VALUES('admin', '40.441725', '-79.953776', '../IMG/posvar.png')") or die ("Invalid: " . $db->error); //test post
+  $db->query("INSERT INTO Pics (uploaderId, picLat, picLng, picPath) VALUES('admin', '40.444272', '-79.953172', '../IMG/cathedral.png')") or die ("Invalid: " . $db->error); //test post
+  $db->query("INSERT INTO Pics (uploaderId, picLat, picLng, picPath) VALUES('admin', '40.442582', '-79.956709', '../IMG/towers.png')") or die ("Invalid: " . $db->error); //test post
   //$db->query('INSERT INTO Users (id,user,interests,education)VALUES("1", "neel","football movies","University of Pittsburgh")') or die ("Invalid: " . $db->error); //test for user info
   
-=======
-  $result = $db->query("create table Posts (id int, user char(30) not null, type char(30) not null, content char(30), location char(30), PRIMARY KEY (user)))") or die ("Invalid: " . $db->error);//Posts table
-  $result = $db->query("create table Users (id int, user char(30) not null, interests char(100) not null, education char(100), PRIMARY KEY (user))") or die ("Invalid: " . $db->error);//Users Table
- 
- //dummy data for testing
-  $db->query('INSERT INTO Posts (id, user, type, content, location)VALUES("1", "neel","text","hello world", "Pittsburgh, PA")') or die ("Invalid: " . $db->error); //test post
-  $db->query('INSERT INTO Users (id,user,interests,education)VALUES("1", "neel","football movies","University of Pittsburgh")') or die ("Invalid: " . $db->error); //test for user info
->>>>>>> master
   echo "<h3>Here is your data:</h3>";
   $query = "select * from Pics;";
   $result = $db->query($query);
